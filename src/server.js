4c258ac0d48ff0b00219f33e597623d8b2f181d5
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-27 20:35:20 -0300

    feat: create local database

--- src/server.js
+++ src/server.js
@@ -1,38 +1,32 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
+import { json } from "./middlewares/json.js";
+import { Database } from "./middlewares/database.js";
 
-const tasks = [];
+const database = new Database();
 
 const server = http.createServer(async (request, response) => {
-  const buffers = [];
-
-  for await (const chunck of request) {
-    buffers.push(chunck);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    request.body = null;
-  }
-
   const { method, url } = request;
 
+  await json(request, response);
+
   if (method === "GET" && url === "/tasks") {
-    return response
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(tasks));
+    const tasks = database.select("tasks");
+    return response.end(JSON.stringify(tasks));
   }
   if (method === "POST" && url === "/tasks") {
     const { title, description, completed_at, created_at, updated_at } =
       request.body;
-    tasks.push({
-      id: 1,
+    const tasks = {
+      id: randomUUID(),
       title,
       description,
       completed_at,
       created_at,
       updated_at,
-    });
+    };
+
+    database.insert("tasks", tasks);
     return response.writeHead(201).end();
   }
 
