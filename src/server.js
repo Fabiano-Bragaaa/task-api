26e122d53c81656acde3b5adc73d10037081d568
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-27 14:42:02 -0300

    feat: create inicial routes for test

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
-import http from 'node:http'
+import http from "node:http";
+
+const tasks = [];
 
 const server = http.createServer((request, response) => {
-  return response.end('helo word')
-})
+  const { method, url } = request;
 
-server.listen(3333)
+  if (method === "GET" && url === "/tasks") {
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(tasks));
+  }
+  if (method === "POST" && url === "/tasks") {
+    tasks.push({
+      id: 1,
+      title: "ola",
+      description: "essa task é nova",
+      completed_at: null,
+      created_at: "2025-05-25T10:00:00Z",
+      updated_at: "2025-05-25T10:00:00Z",
+    });
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end("Not found");
+});
+
+server.listen(3333);
